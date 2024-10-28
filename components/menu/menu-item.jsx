cf3abd0cb5dc4b870f0a9524c540d88f1daa3c71
Rodrigo9602 <fernandezrodrigo9602@gmail.com>
2024-10-28 13:16:05 -0400

    Update menu-item.jsx

--- components/menu/menu-item.jsx
+++ components/menu/menu-item.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MenuItem = ({className, children}) => {
     return (
         <>
-            <button className={`px-2 py-3 gap-2 font-roboto font-medium text-base text-white leading-6 tracking-tight ${className}`}>
+            <button className={`px-2 py-3 gap-2 font-roboto font-medium text-base text-white leading-6 tracking-[0.5px] ${className}`}>
                 {children}
             </button>
         </>
