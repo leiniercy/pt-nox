61f771b50319b510436405dcf454823513381623
Rodrigo9602 <fernandezrodrigo9602@gmail.com>
2024-10-30 16:55:45 -0400

    MOD: Added navbar link animation

--- components/menu/menu-item.jsx
+++ components/menu/menu-item.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MenuItem = ({className, children}) => {
     return (
         <>
-            <button className={`px-2 py-3 gap-2 font-roboto font-medium text-base text-white leading-6 tracking-[0.5px] ${className}`}>
+            <button className={`px-2 py-3 gap-2 font-roboto font-medium text-base text-white leading-6 tracking-[0.5px] transition-colors duration-300 ease-in-out hover:text-[#4f46e5] ${className}`}>
                 {children}
             </button>
         </>
