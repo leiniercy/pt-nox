94e41d78ae20bfde9c1c84624325aba7e4e901ed
Rodrigo9602 <fernandezrodrigo9602@gmail.com>
2024-10-30 05:03:05 -0400

    MOD: Section 7 styles fix on tablet resolution

--- views/seccion7/index.jsx
+++ views/seccion7/index.jsx
@@ -8,8 +8,8 @@ const Seccion7 = ({ className }) => {
     return (
         <>
             <main className={`w-full flex items-center justify-center py-6 px-4 md:p-24 + ${className}`}>
-                <section className='w-full md:w-3/4 flex flex-col md:flex-row items-center justify-center border rounded-[20px] py-6 md:py-16 md:pl-16 border-[#475569]'>
-                    <div className='w-full md:w-1/2 flex flex-col items-start justify-center gap-8 px-4'>
+                <section className='w-full lg:w-[85%] flex flex-col lg:flex-row items-center justify-center border rounded-[20px] py-6 md:py-12 md:pl-8 lg:py-16 lg:pl-16 border-[#475569]'>
+                    <div className='w-full lg:w-1/2 flex flex-col items-start justify-center gap-8 px-4'>
                         <h1 className='font-roboto font-medium text-[18px] leading-[19.8px] md:font-bold md:text-[32px] md:leading-[35.2px] text-white'>
                             {texts.title}
                         </h1>
@@ -17,18 +17,18 @@ const Seccion7 = ({ className }) => {
                             {texts.paragraph}
                         </p>
                         <CustomButton
-                            className='flex-1 p-4 md:flex-none w-[245px] md:w-[237px]'
+                            className='flex-1 p-4 lg:flex-none w-[245px] lg:w-[237px]'
                             cancel
                             name={texts.botonRequest} 
                             textSize={20}/>
                     </div>
-                    <div className='md:w-1/2 flex items-center justify-center'>
+                    <div className='lg:w-1/2 flex items-center justify-center'>
                         <Image
                             src="/assets/images/seccion7/picture-0.svg"
                             width={608}
                             height={277}
                             alt="graph-img"
-                            className='w-[320px] h-[228px] md:w-[608px] md:h-[277px]'
+                            className='w-[320px] h-[228px] sm:w-[608px] sm:h-[277px]'
                         />
                     </div>
                 </section>
